b0feea72d3cf4d02f93dd8f1f56b327f1bdd0e25
sisu-01 <tesv010615@naver.com>
2026-06-08 13:56:58 +0900

    색 고정

--- src/components/Cube.tsx
+++ src/components/Cube.tsx
@@ -9,20 +9,21 @@ const Cube = ({ size }: CubeProps) => {
   const [hoveredCoords, setHoveredCoords] = useState<{x: number, y: number, z: number} | null>(null);
 
   const cubes = [];
+  let index = 0;
   for (let x = 0; x < size; x++) {
     for (let y = 0; y < size; y++) {
       for (let z = 0; z < size; z++) {
-        cubes.push({ x, y, z });
+        const lightness = (index + 1) / (size ** 3 / 100);
+        cubes.push({x, y, z, initialLightness: lightness });
+        index++;
       }
     }
   }
 
   return (
     <>
-      {cubes.map((pos, posIndex) => {
+      {cubes.map((pos) => {
         const id = `${pos.x}${pos.y}${pos.z}`;
-        // lightness 계산: 0~1 사이의 값을 얻기 위해 나눗셈 조정
-        const lightness = (posIndex+1) / (size**3/100);
 
         return (
           <CubeLet
@@ -31,7 +32,7 @@ const Cube = ({ size }: CubeProps) => {
             x={pos.x}
             y={pos.y}
             z={pos.z}
-            lightness={lightness}
+            lightness={pos.initialLightness}
             hoveredCoords={hoveredCoords}
             onHover={(hx, hy, hz) => setHoveredCoords({ x: hx, y: hy, z: hz })}
             onHoverOut={() => setHoveredCoords(null)}
